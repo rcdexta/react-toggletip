89347cbe56315e16d1dc2eaa21df740cd05a1559
RC <rc.chandru@gmail.com>
2018-05-01 08:40:36 -0700

    feat(component): Publishing first set of features that include placement and storybook examples

---
+++ components/ToggleTip.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import InfoIcon from 'react-icons/lib/fa/info-circle'
+import './styles'
+
+class ToggleTip extends React.Component {
+  get label() {
+    const {content} = this.props
+    if (content instanceof Array) {
+      return content.join('\n')
+    } else {
+      return content
+    }
+  }
+
+  get ttlpClasses() {
+    const {animated, placement} = this.props
+    const classes = ['tltp--rounded']
+    if (!animated) classes.push('tltp--no-animate')
+    classes.push(`tltp--${placement}`)
+    return classes.join(' ')
+  }
+
+  render() {
+    return (
+      <span className={this.ttlpClasses} style={{display: 'flex'}} aria-label={this.label}>
+        <InfoIcon style={{cursor: 'pointer', verticalAlign: 'none'}} />
+      </span>
+    )
+  }
+}
+
+ToggleTip.propTypes = {
+  content: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  placement: PropTypes.oneOf([
+    'bottom-right',
+    'bottom',
+    'bottom-left',
+    'right',
+    'left',
+    'top-right',
+    'top',
+    'top-left'
+  ]),
+  animated: PropTypes.bool
+}
+
+ToggleTip.defaultProps = {
+  placement: 'bottom',
+  animated: true
+}
+
+export default ToggleTip
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import {storiesOf, addDecorator} from '@storybook/react'
+import {linkTo} from '@storybook/addon-links'
+import {Welcome} from '@storybook/react/demo'
+import ToggleTip from '../components/ToggleTip'
 
-import { Button, Welcome } from '@storybook/react/demo';
+addDecorator(story => <div style={{margin: 30}}>{story()}</div>)
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('ToggleTip', module).add('Basic', () => {
+  return (
+    <div className="elements">
+      <div className="btn-group">
+        <div className="btn two">
+          <span>Net Price</span>
+          <ToggleTip content="Price of item before tax" />
+        </div>
+        <div className="btn two">
+          <span>Gross Price</span>
+          <ToggleTip content="Price of item including tax and service charges" />
+        </div>
+      </div>
+    </div>
+  )
+})
+
+storiesOf('ToggleTip', module).add('Placement', () => {
+  return (
+    <div className="elements">
+      <div className="btn-group">
+        <div className="btn">
+          <ToggleTip content="Show tooltip to Left" placement="left" />
+          <span>Left</span>
+        </div>
+        <div className="btn two">
+          <span>Top</span>
+          <ToggleTip content="Show tooltip to top" placement="top" />
+        </div>
+        <div className="btn three">
+          <span>Bottom</span>
+          <ToggleTip content="Show tooltip to bottom" placement="bottom" />
+        </div>
+        <div className="btn four">
+          <span>Right</span>
+          <ToggleTip content="Show tooltip to right" placement="right" />
+        </div>
+      </div>
+    </div>
+  )
+})
